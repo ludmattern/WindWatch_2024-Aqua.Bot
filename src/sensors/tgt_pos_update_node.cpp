d45455c728c19bffb33d666d558af4562ca83c2c
ludmattern <github.rfgrg@passmail.net>
2024-10-20 17:11:36 +0200

    added the tgt_pos_update_node

---
+++ src/sensors/tgt_pos_update_node.cpp
@@ -0,0 +1,5 @@
+// Fichier source pour le nœud de recuperations des positions des cibles
+int main(int argc, char **argv) {
+    // TODO: Implémentation du nœud de recuperations des positions des cibles
+    return 0;
+}
