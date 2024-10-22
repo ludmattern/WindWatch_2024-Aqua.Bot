157012915165ad59a0a95f590f62ead4b692cde7
pyven-dr <pyven-dr@student.42lyon.fr>
2024-10-22 19:20:21 +0000

    start path finding node

--- src/navigation/include/navigation/path_planning_node.hpp
+++ src/navigation/include/navigation/path_planning_node.hpp
@@ -2,11 +2,23 @@
 #define PATH_PLANNING_NODE_HPP
 
 #include <rclcpp/rclcpp.hpp>
+#include "geometry_msgs/msg/pose_array.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 
 class PathPlanningNode : public rclcpp::Node
 {
-public:
-    PathPlanningNode();
+    public:
+        PathPlanningNode();
+
+    private:
+
+        std::vector<std::pair<double, double>> PointList;
+        std::vector<std::vector<double>> Graph;
+
+        rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr tgtPos_Subscirption_;
+        rclcpp::Subscription<>
+
+        void FillPtsList(void);
 };
 
 #endif // PATH_PLANNING_NODE_HPP
