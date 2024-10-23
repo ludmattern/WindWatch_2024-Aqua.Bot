b4e8978885a9a16154e9cc7e7b4361f98b561dc3
pyven-dr <pyven-dr@student.42lyon.fr>
2024-10-23 13:41:36 +0000

    Change TgtPosUtpdate from a topic to a service

--- src/sensors/src/camera_processing_node.cpp
+++ src/sensors/src/camera_processing_node.cpp
@@ -50,7 +50,7 @@ void CameraProcessingNode::targetPoseCallback(void)
 		theta_relative += 2 * M_PI;
 	}
 
-	RCLCPP_INFO(this->get_logger(), "Angle relatif de la caméra : %f rad", theta_relative);
+	//RCLCPP_INFO(this->get_logger(), "Angle relatif de la caméra : %f rad", theta_relative);
 
 	// Ici, orienter la caméra en fonction de theta_relative
 	controlCamera(theta_relative);
--- src/sensors/src/tgt_pos_update_node.cpp
+++ src/sensors/src/tgt_pos_update_node.cpp
@@ -12,34 +12,41 @@ TgtPosUpdateNode::TgtPosUpdateNode() : Node("tgt_pos_update_node")
 		"/aquabot/ais_sensor/windturbines_positions", 10,
 		std::bind(&TgtPosUpdateNode::tgtPosCallBack, this, std::placeholders::_1));
 
-	//Create publisher
-	tgtPos_Publisher_ = this->create_publisher<geometry_msgs::msg::PoseArray>(
-		"/mission/target_positions", 10);
+	//Create Service
+	tgtPos_Service_ = this->create_service<sensors::srv::TargetPositions>(
+		"mission/target_positions", std::bind(&TgtPosUpdateNode::ServerCallback, this, std::placeholders::_1, std::placeholders::_2));
 
 }
 
 void TgtPosUpdateNode::tgtPosCallBack(geometry_msgs::msg::PoseArray msg)
 {
-	geometry_msgs::msg::PoseArray msgPublisher;
-
-	msgPublisher.poses.resize(msg.poses.size()); //resize the vector
-	for (int i = 0; i < msg.poses.size(); ++i)
+	if (current_positions_.poses.empty())
 	{
-		double x,y,z;
-		latLonToENU(msg.poses[i].position.x ,msg.poses[i].position.y, msg.poses[i].position.z, x, y, z); //convert coordinates
+		current_positions_.poses.resize(msg.poses.size()); //resize the vector
+		for (int i = 0; i < msg.poses.size(); ++i)
+		{
+			double x,y,z;
+			latLonToENU(msg.poses[i].position.x ,msg.poses[i].position.y, msg.poses[i].position.z, x, y, z); //convert coordinates
 
-		//Set topic msg position to target position
-		msgPublisher.poses[i].position.x = x;
-		msgPublisher.poses[i].position.y = y;
-		msgPublisher.poses[i].position.z = z;
+			//Set position to target position
+			current_positions_.poses[i].position.x = x;
+			current_positions_.poses[i].position.y = y;
+			current_positions_.poses[i].position.z = z;
 
-		//Set topic msg rotation to 0
-		msgPublisher.poses[i].orientation.w = 0;
-		msgPublisher.poses[i].orientation.x = 0;
-		msgPublisher.poses[i].orientation.y = 0;
-		msgPublisher.poses[i].orientation.z = 0;
+			//Set rotation to 0
+			current_positions_.poses[i].orientation.w = 0;
+			current_positions_.poses[i].orientation.x = 0;
+			current_positions_.poses[i].orientation.y = 0;
+			current_positions_.poses[i].orientation.z = 0;
+		}
 	}
-	tgtPos_Publisher_->publish(msgPublisher); //Publish message on topic
+}
+
+void TgtPosUpdateNode::ServerCallback(const std::shared_ptr<sensors::srv::TargetPositions::Request> request,
+	const std::shared_ptr<sensors::srv::TargetPositions::Response> response)
+{
+	//Set response to the target positions.
+	response->poses = current_positions_;
 }
 
 int main(int argc, char * argv[])
