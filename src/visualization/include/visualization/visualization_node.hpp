b0ec123949d5524e4aee9695751e3f43e0b5cfeb
zigeuh <baronthirolle.mael@gmail.com>
2024-10-23 16:12:25 +0000

    First test of putting elements in Rivz2

--- src/visualization/include/visualization/visualization_node.hpp
+++ src/visualization/include/visualization/visualization_node.hpp
@@ -2,11 +2,25 @@
 #define VISUALIZATION_NODE_HPP
 
 #include <rclcpp/rclcpp.hpp>
+#include "nav_msgs/msg/odometry.hpp"
+#include <visualization_msgs/msg/marker.hpp>
+#include "geometry_msgs/msg/point_stamped.hpp"
 
 class VisualizationNode : public rclcpp::Node
 {
 public:
-    VisualizationNode();
+	VisualizationNode();
+
+private:
+
+	void odometryCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
+
+    // Subscribtions
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odometry_subscription_;
+
+	// Publishers
+	rclcpp::Publisher<geometry_msgs::msg::PointStamped>::SharedPtr marker_publisher_;
+
 };
 
 #endif // VISUALIZATION_NODE_HPP
--- src/visualization/src/visualization_node.cpp
+++ src/visualization/src/visualization_node.cpp
@@ -1,9 +1,31 @@
 // obstacle_avoidance_node.cpp
+
 #include "visualization/visualization_node.hpp"
 
 VisualizationNode::VisualizationNode() : Node("visualization_node")
 {
-    RCLCPP_INFO(this->get_logger(), "Visualization Node has started");
+    // Subscriptions
+    odometry_subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
+        "/mission/odometry", 10,
+        std::bind(&VisualizationNode::odometryCallback, this, std::placeholders::_1));
+
+    marker_publisher_ = this->create_publisher<geometry_msgs::msg::PointStamped>("/minimaps/test", 10);
+
+    geometry_msgs::msg::PointStamped Point;
+
+	Point.header.frame_id = "odom";
+	Point.header.stamp = this->get_clock()->now();
+	Point.point.set__x(0.0);
+	Point.point.set__y(0.0);
+	Point.point.set__z(0.0);
+	marker_publisher_->publish(Point);
+
+	RCLCPP_INFO(this->get_logger(), "Visualization Node has started");
+}
+
+void VisualizationNode::odometryCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
+{
+    return ;
 }
 
 int main(int argc, char * argv[])
