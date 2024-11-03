50cecc832fa93c666c79ee5b08cdbaaaaa418fd7
ludmattern <github.rfgrg@passmail.net>
2024-11-03 15:24:40 +0000

    merged the target_manager_node from alban branch

---
+++ src/mission_manager/include/mission_manager/mission_coordinator_node.hpp
@@ -0,0 +1,12 @@
+#ifndef MISSION_COORDINATOR_NODE_HPP
+#define MISSION_COORDINATOR_NODE_HPP
+
+#include <rclcpp/rclcpp.hpp>
+
+class MissionCoordinatorNode : public rclcpp::Node
+{
+public:
+    MissionCoordinatorNode();
+};
+
+#endif // MISSION_COORDINATOR_NODE_HPP
